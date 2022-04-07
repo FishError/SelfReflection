d6f97b18575356d325dcb88f1ff2473a34dd0118
Jacky Chen <xcrowli12@gmail.com>
2022-04-06 23:41:21 -0400

    Fixed cube movement through mirror

--- SelfReflection/SelfReflection/Assets/Scripts/Raycast/RaycastPickup.cs
+++ SelfReflection/SelfReflection/Assets/Scripts/Raycast/RaycastPickup.cs
@@ -12,26 +12,49 @@ public class RaycastPickup : MonoBehaviour
     [Header("Physics Parameters")]
     [SerializeField] private float pickupRange = 5.0f;
     [SerializeField] private float pickupForce = 150.0f;
+    public RaycastHit hit;
+    private Ray ray;
+    private float remainingLength = 1000;
+    private float mouseX, mouseY;
+    public float sensX;
+    public float sensY;
+
 
     private void Update()
     {
+        mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
         if (Input.GetMouseButtonDown(0))
         {
-            if(heldObject == null)
+            if (heldObject == null)
             {
-                RaycastHit hit;
-                if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, pickupRange))
+                ray = new Ray(transform.position, transform.TransformDirection(Vector3.forward));
+                for (int i = 0; i < 5; i++)
                 {
-                    //Pick Up Object
-                    print("Hit");
-                    PickupObject(hit.transform.gameObject);
+                    if (Physics.Raycast(ray.origin, ray.direction, out hit, remainingLength))
+                    {
+                        remainingLength -= Vector3.Distance(ray.origin, hit.point);
+                        ray = new Ray(hit.point, Vector3.Reflect(ray.direction, hit.normal));
+                        if (hit.collider.CompareTag("Cube"))
+                        {
+                            PickupObject(hit.transform.gameObject);
+
+                        }
+                        if (hit.collider.tag != "Mirror")
+                        {
+                            break;
+                        }
+                    }
                 }
             }
             else
             {
-                //Drop Object
                 DropObject();
             }
+        }
+        if (heldObject != null)
+        {
+            heldObjectrb.transform.position = new Vector3(heldObjectrb.transform.position.x, heldObjectrb.transform.position.y + mouseY, heldObjectrb.transform.position.z + mouseX);
         }
     }
 
@@ -42,10 +65,9 @@ public class RaycastPickup : MonoBehaviour
         {
             heldObjectrb = obj.GetComponent<Rigidbody>();
             heldObjectrb.useGravity = false;
-            heldObjectrb.drag = 5;
+            heldObjectrb.drag = 10;
             heldObjectrb.constraints = RigidbodyConstraints.FreezeRotation;
 
-            heldObjectrb.transform.parent = holdArea;
             heldObject = obj;
         }
     }
