f9a1d112a7252e211b45d4c2e26f76d4798df503
Jacky Chen <xcrowli12@gmail.com>
2022-04-22 18:07:16 -0400

    Modify model and added colliders

--- SelfReflection/SelfReflection/Assets/Scripts/PlayerMovement/PlayerMovement.cs
+++ SelfReflection/SelfReflection/Assets/Scripts/PlayerMovement/PlayerMovement.cs
@@ -64,6 +64,7 @@ public class PlayerMovement : MonoBehaviour
     {
         // ground check
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, Ground);
+        print(grounded);
 
         // check for ledge
         ledgeCheck1 = Physics.Raycast(new Vector3(transform.position.x, transform.position.y + playerHeight - 0.1f, transform.position.z), transform.forward, out ledge, 1.2f, Ground);
@@ -74,7 +75,9 @@ public class PlayerMovement : MonoBehaviour
 
         // drag
         if (grounded)
+        {
             rb.drag = groundDrag;
+        }  
         else
             rb.drag = 0;
     }
