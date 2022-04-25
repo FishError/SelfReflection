cd2ab8d006541fd054de012eba262bf013a03953
Jacky Chen <xcrowli12@gmail.com>
2022-04-24 21:52:17 -0400

    added player movement speed + climb up speed for parameter managers and saving/loading of those parameters

--- SelfReflection/SelfReflection/Assets/Scripts/UI_Navigation/SaveParameters.cs
+++ SelfReflection/SelfReflection/Assets/Scripts/UI_Navigation/SaveParameters.cs
@@ -1,8 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SaveParameters : MonoBehaviour
 {
-    
+
+    public Slider playerMass, playerJumpForce, playerSpeed, playerClimbSpeed;
+    public void SavePlayerMass(float mass)
+    {
+        playerMass.value = mass;
+        PlayerPrefs.SetFloat("PlayerMass", mass);
+    }
+
+    public void SavePlayerJumpForce(float force)
+    {
+        playerJumpForce.value = force;
+        PlayerPrefs.SetFloat("PlayerJumpforce", force);
+    }
+
+    public void SavePlayerSpeed(float speed)
+    {
+        playerSpeed.value = speed;
+        PlayerPrefs.SetFloat("PlayerSpeed", speed);
+    }
+
+    public void SavePlayerClimbSpeed(float climbSpeed)
+    {
+        playerClimbSpeed.value = climbSpeed;
+        PlayerPrefs.SetFloat("ClimbSpeed", climbSpeed);
+    }
 }
