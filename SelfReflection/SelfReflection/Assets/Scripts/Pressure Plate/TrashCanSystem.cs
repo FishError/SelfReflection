88893d7cc4293033cb064d8377921b133a1d1e25
Jacky Chen <jc4283@drexel.edu>
2022-05-11 02:09:02 -0400

    fixed trash can mechs

--- SelfReflection/SelfReflection/Assets/Scripts/Pressure Plate/TrashCanSystem.cs
+++ SelfReflection/SelfReflection/Assets/Scripts/Pressure Plate/TrashCanSystem.cs
@@ -7,17 +7,40 @@ public class TrashCanSystem : MonoBehaviour
 {
     public Vector3 lidPosition;
     public Vector3 lidRotation;
+    public Vector3 lidScale;
     public GameObject lid;
+    public int initLayer, finalLayer;
+    public bool isCovered;
+    public List<GameObject> objectToDisable;
+    public int NumOfObjectToThrow = 0;
+    private int countObjectThrown;
+
+    private void Start()
+    {
+        isCovered = false;
+        lid.gameObject.layer = initLayer;
+        countObjectThrown = 0;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player" && isCovered == false)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
-        else if(other.gameObject.name == "ThrowObject")
+        else if(other.gameObject.name.Contains("ThrowObject"))
         {
-            lid.transform.localPosition = lidPosition;
-            lid.transform.localEulerAngles = lidRotation;
+            objectToDisable[countObjectThrown].SetActive(false);
+            countObjectThrown++;
+
+            if (countObjectThrown == NumOfObjectToThrow)
+            {
+                lid.transform.localPosition = lidPosition;
+                lid.transform.localEulerAngles = lidRotation;
+                lid.transform.localScale = lidScale;
+                lid.gameObject.layer = finalLayer;
+                isCovered = true;
+            }
         }
     }
 }
