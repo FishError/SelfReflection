f77c80122b07c7a18eacab2d5a408f64c9fc6bb2
Jacky Chen <xcrowli12@gmail.com>
2022-05-17 02:39:05 -0400

    changing reset object manager

--- SelfReflection/SelfReflection/Assets/Scripts/Object Mechanics/ResetObjectManager.cs
+++ SelfReflection/SelfReflection/Assets/Scripts/Object Mechanics/ResetObjectManager.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class ResetObjectManager : MonoBehaviour
 {
-    public GameObject northWall, southWall, leftWall, rightWall;
     public Dictionary<GameObject, Vector3> objPosition = new Dictionary<GameObject, Vector3>();
     public List<GameObject> interactableObj = new List<GameObject>();
     public MoveObjectController objectController;
 
     private void Start()
     {
-        if(interactableObj.Count == 0)
+
+        if (interactableObj.Count == 0)
         {
             return;
         }
@@ -35,63 +35,11 @@ public class ResetObjectManager : MonoBehaviour
             for (int i = 0; i < interactableObj.Count; i++)
             {
 
-                /*
-                CheckLeftWall(i);
-                CheckRightWall(i);
-                CheckNorthWall(i);
-                CheckSouthWall(i);*/
+               
             }
         }
-        
+
+
+
     }
-
-
-    /*
-    public void CheckLeftWall(int i)
-    {
-        if (interactableObj[i].transform.position.x < leftWall.transform.position.x)
-        {
-            if (objPosition.ContainsKey(interactableObj[i]))
-            {
-                interactableObj[i].transform.position = objPosition[interactableObj[i]];
-                objectController.DropObject();
-            }
-        }
-    }
-
-    public void CheckRightWall(int i)
-    {
-        if (interactableObj[i].transform.position.x > rightWall.transform.position.x)
-        {
-            if (objPosition.ContainsKey(interactableObj[i]))
-            {
-                interactableObj[i].transform.position = objPosition[interactableObj[i]];
-                objectController.DropObject();
-            }
-        }
-    }
-
-    public void CheckNorthWall(int i)
-    {
-        if (interactableObj[i].transform.position.z > northWall.transform.position.z)
-        {
-            if (objPosition.ContainsKey(interactableObj[i]))
-            {
-                interactableObj[i].transform.position = objPosition[interactableObj[i]];
-                objectController.DropObject();
-            }
-        }
-    }
-
-    public void CheckSouthWall(int i)
-    {
-        if (interactableObj[i].transform.position.z < southWall.transform.position.z)
-        {
-            if (objPosition.ContainsKey(interactableObj[i]))
-            {
-                interactableObj[i].transform.position = objPosition[interactableObj[i]];
-                objectController.DropObject();
-            }
-        }
-    }*/
 }
