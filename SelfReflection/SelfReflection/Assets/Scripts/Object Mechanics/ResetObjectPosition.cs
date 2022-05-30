53ea9fd91bcd0b6608ed6a2330d077eb484c4029
Jacky Chen <xcrowli12@gmail.com>
2022-05-30 02:41:03 -0400

    death cam next time

--- SelfReflection/SelfReflection/Assets/Scripts/Object Mechanics/ResetObjectPosition.cs
+++ SelfReflection/SelfReflection/Assets/Scripts/Object Mechanics/ResetObjectPosition.cs
@@ -10,7 +10,6 @@ public class ResetObjectPosition : MonoBehaviour
     private GameObject pickUpParent;
     private float time;
     public SkinnedMeshRenderer meshRenderer;
-    public Camera deathCam;
     private bool isDead = false;
     private bool isAlive = false;
     private float amt = 0.7f;
