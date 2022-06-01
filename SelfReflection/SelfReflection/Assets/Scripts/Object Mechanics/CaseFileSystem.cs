d1b7cfa664fb8a0cafb41ba1df43dd789129a398
Jacky Chen <jc4283@drexel.edu>
2022-06-01 01:38:23 -0400

    added casefile mechanics to time challenge 1, not completed for time challenge 2 or 3, casefilesystem script not completed as well

--- SelfReflection/SelfReflection/Assets/Scripts/Object Mechanics/CaseFileSystem.cs
+++ SelfReflection/SelfReflection/Assets/Scripts/Object Mechanics/CaseFileSystem.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CaseFileSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject fileCam;
+    public TextMeshProUGUI text;
+    public GameObject alarmClockCanvas;
+    private Animator anim;
+    private PlayerMovement playerMovement;
+    private GameObject player;
+
+    private void Start()
     {
-        
+        anim = this.transform.parent.GetComponent<Animator>();
+        playerMovement = GameObject.Find("Player").GetComponent<PlayerMovement>();
+        player = GameObject.Find("Player");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if(other.tag == "Player")
+        {
+            fileCam.SetActive(true);
+            anim.SetBool("isTouching", true);
+            playerMovement.DisableMovement();
+            this.transform.parent.localEulerAngles = new Vector3(0, 90, 0);
+            player.SetActive(false);
+            alarmClockCanvas.GetComponent<AlarmClock>().timerIsRunning = false;
+            alarmClockCanvas.transform.GetChild(1).gameObject.SetActive(false);
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
     }
 }
