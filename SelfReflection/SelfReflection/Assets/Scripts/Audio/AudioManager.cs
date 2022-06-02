3f67c9d9fd9e095c57308d0ac2c8d8ed45d3e9ac
jmk555@tux.cs.drexel.edu <61361783+JayIKarus@users.noreply.github.com>
2022-06-02 13:54:53 -0400

    Audio Update
    Added Dialogue to all three hubs and the time challenges.

---
+++ SelfReflection/SelfReflection/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public GameObject[] audioObjects;
+    public GameObject currentAudioObject;
+    public float playTime = 0f;
+    public bool startPlayback = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentAudioObject = audioObjects[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startPlayback)
+        {
+            play();
+            release();
+        }
+        setPlayback(false);
+    }
+
+    public void setCurAudioObject(GameObject audio)
+    {
+        currentAudioObject = audio;
+    }
+    private void play()
+    {
+        currentAudioObject.SetActive(true);
+        
+    }
+
+    private void release()
+    {
+        currentAudioObject.SetActive(false);
+    }
+
+    public void setPlayback(bool a)
+    {
+        startPlayback = a;
+    }
+}
