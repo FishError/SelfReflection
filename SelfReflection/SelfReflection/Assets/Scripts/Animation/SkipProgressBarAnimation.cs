af85a4aef8c704c23eb6ec944417d9602e609f50
Jacky Chen <xcrowli12@gmail.com>
2022-07-10 13:15:35 -0400

    added skipping camera panning function + UI visualization for camera skipping

---
+++ SelfReflection/SelfReflection/Assets/Scripts/Animation/SkipProgressBarAnimation.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkipProgressBarAnimation : MonoBehaviour
+{
+    private float amt = 0.7f;
+    public GameObject curProgress;
+    public bool isPlaying = false;
+
+    public void EnableBarAnimation()
+    {
+        if(curProgress.transform.localScale.x < 1)
+        {
+            curProgress.transform.localScale += new Vector3(amt * Time.deltaTime, 0, 0);
+            isPlaying = true;
+        }
+        else
+        {
+            isPlaying = false;
+        }
+    }
+}
