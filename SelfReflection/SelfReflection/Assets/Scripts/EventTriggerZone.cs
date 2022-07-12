5ed1470c877a2a461976203c95aee73c8cc85ff1
alexhung <alexh4949@gmail.com>
2022-07-11 23:08:41 -0400

    removed unneccesary code

--- SelfReflection/SelfReflection/Assets/Scripts/EventTriggerZone.cs
+++ SelfReflection/SelfReflection/Assets/Scripts/EventTriggerZone.cs
@@ -8,18 +8,6 @@ public class EventTriggerZone : MonoBehaviour
 
     public UnityEvent myEvent;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if (myEvent != null)
