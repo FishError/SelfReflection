c8459b9cde19e300ad38e7a49ee4c6cdf000de31
Ben Tang <sjps30503@gmail.com>
2022-07-13 18:39:17 -0400

    Update Button_Nav.cs

--- SelfReflection/SelfReflection/Assets/Scripts/UI_Navigation/Button_Nav.cs
+++ SelfReflection/SelfReflection/Assets/Scripts/UI_Navigation/Button_Nav.cs
@@ -10,17 +10,24 @@ public class Button_Nav : MonoBehaviour
     public Button button;
     public GameObject newDisplay;
     public int sceneNum = 1;
+
     // Start is called before the first frame update
     void Start()
     {
         Button myControl = button.GetComponent<Button>();
         myControl.onClick.AddListener(TaskOnClick);
+
+        // If there is no Save file, continue button should not be interactable
+        if (button.name == "Continue_Button" && !SaveSystem.HasSaveFile)
+        {
+            button.interactable = false;
+            GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-
     }
 
     void TaskOnClick()
@@ -31,9 +38,22 @@ public class Button_Nav : MonoBehaviour
         }
         else if (button.name == "Start_Button")
         {
-            int nextSceneIndex = (int)SaveSystem.Load().LastVisitedLevel;
+            // Start button should go to demo stage 1 by default
+            SceneManager.LoadScene((int) SceneIndex.DemoStage1);
+            Time.timeScale = 1;
+        }
+        else if (button.name == "Continue_Button")
+        {
+            // Safe-guarding
+            if (!SaveSystem.HasSaveFile)
+            {
+                Debug.LogError("The file Saves.json does not exist.");
+                return;
+            }
+
+            // Continue button should go to the last visited level
+            int nextSceneIndex = (int) SaveSystem.Load().LastVisitedLevel;
             SceneManager.LoadScene(nextSceneIndex);
-            // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+sceneNum);
             Time.timeScale = 1;
         }
         else if (button.name == "MainMenu_Button")
