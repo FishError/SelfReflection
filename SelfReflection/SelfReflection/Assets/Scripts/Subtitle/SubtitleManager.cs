42a0b637f0df588b2f89af63306d7adbdd26f62a
linli56304 <linli56304@gmail.com>
2022-07-26 12:03:07 -0400

    subtitleManager.addSubtitle(audioObjects[currentAudioObject]);

--- SelfReflection/SelfReflection/Assets/Scripts/Subtitle/SubtitleManager.cs
+++ SelfReflection/SelfReflection/Assets/Scripts/Subtitle/SubtitleManager.cs
@@ -19,6 +19,9 @@ public class SubtitleManager : MonoBehaviour
         // Update is called once per frame
         void Update()
     {
+        if (subtitles != null) {
+            playSubtitle();
+        }
         
     }
 
