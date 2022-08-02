38d1aabb828699cb6633fe52b1034a9618416fdf
linli56304 <linli56304@gmail.com>
2022-08-02 12:13:24 -0400

    added audioLengthPair class

--- SelfReflection/SelfReflection/Assets/Scripts/Audio/AudioManager.cs
+++ SelfReflection/SelfReflection/Assets/Scripts/Audio/AudioManager.cs
@@ -10,27 +10,34 @@ using FMODUnity;
 public class AudioManager : MonoBehaviour
 {
     [Serializable]
-    public class KeyValuePair
+    public class audioSubtitlePair
     {
         public GameObject audioObject;
-        public string subtitleText;
+        public Dictionary<string, float>subtitleDictiontionary;
     }
 
-    public List<KeyValuePair> audioList = new List<KeyValuePair>();
-    Dictionary<GameObject, string> audioObjects = new Dictionary<GameObject, string>();
+    public class audioLengthPair
+    {
+        public string subtitle;
+        public float length;
+    }
+
+    public List<audioSubtitlePair> audioList = new List<audioSubtitlePair>();
+    Dictionary<GameObject, Dictionary<string, float>> audioObjects = new Dictionary<GameObject, Dictionary<string, float>>();
     public GameObject currentAudioObject;
-    public float audioLength = 0f;
+    private int audioLength = 10;
     public GameObject backgroundMusic;
     public SubtitleManager subtitleManager;
     public float playTime = 0f;
     public bool startPlayback = false;
-    
+
     // Start is called before the first frame update
     void Start()
     {
+        UnityEngine.Debug.Log("start size: " + audioLength);
         foreach (var kvp in audioList)
         {
-            audioObjects[kvp.audioObject] = kvp.subtitleText;
+            audioObjects[kvp.audioObject] = kvp.subtitleDictiontionary;
         }
         backgroundMusic.SetActive(true);
         subtitleManager = GameObject.Find("SubtitleManager").GetComponent<SubtitleManager>(); 
@@ -75,11 +82,17 @@ public class AudioManager : MonoBehaviour
         StudioEventEmitter tempAudio = currentAudioObject.GetComponent<StudioEventEmitter>();
         UnityEngine.Debug.Log(tempAudio);
         tempAudio.EventInstance.getDescription(out EventDescription tempAudioDescription);
-        UnityEngine.Debug.Log(tempAudioDescription.getLength(out int length));
+        //UnityEngine.Debug.Log(tempAudioDescription.getLength(out uint length));
+        //tempAudioDescription.setCallback(EVENT_CALLBACK callback, EVENT_CALLBACK_TYPE callbackmask = EVENT_CALLBACK_TYPE.ALL);
+        //tempAudioDescription.setCallback(tempAudioDescription.getLength(out int tempAudioLength);
+        
         tempAudioDescription.getLength(out int tempAudioLength);
-        UnityEngine.Debug.Log(tempAudioLength);
+        
+        
+        UnityEngine.Debug.Log(tempAudioDescription.isValid());
+        UnityEngine.Debug.Log("old size: " + audioLength);
         audioLength = tempAudioLength;
-        
-        
+        UnityEngine.Debug.Log("new size: " + audioLength);
+
     }
 }
